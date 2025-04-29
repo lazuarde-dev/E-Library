e534e00def836b15479e8f221245e37f5ee07b97
lazuarde-dev <zaky.lazuardi912@gmail.com>
2025-04-29 21:04:35 +0700

    database

---
+++ database/migrations/2025_04_29_134300_create_table_books.php.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_buku');
+            $table->string('penerbit');
+            $table->text('description')->nullable();
+            $table->year('tahun_penerbit');
+            $table->integer('jumlah_halaman');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
